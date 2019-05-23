1c68a5c83add29c04d998024645e26613b89ddda
Daniel Gillen <danielgillen@Daniels-MBP.home>
2019-05-23 10:46:46 -0400

    added new links in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,9 +11,11 @@ const About = (props) => {
           in music performance and music education. I have an affinity for problem
           solving, communication and detail oriented work. My passion for technology
           and people drives me to create the best possible experience
-          for users and colleagues alike.
-          Interested in learning more? <a target="_blank" href="https://drive.google.com/file/d/1l6nI9ruD8orVsiv7xpmfOsk2_O8AgCoy/view?usp=sharing">Checkout My Resume!</a></p>
-
+          for users and colleagues alike.</p>
+          <p>Interested in learning more? Checkout these links</p>
+          <a style={{display: "block"}} target="_blank" href="https://www.linkedin.com/in/danielgillen/">LinkedIn</a>
+          <a target="_blank" href="https://drive.google.com/file/d/1l6nI9ruD8orVsiv7xpmfOsk2_O8AgCoy/view?usp=sharing">Resume</a>
+          <a style={{display: "block"}} target="_blank" href="https://github.com/dgillen27">Github</a>
         </div>
       </div>
     </div>
