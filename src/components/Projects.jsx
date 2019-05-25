c71ac45dd866926150f5ddf4addb7409c6849374
Daniel Gillen <danielgillen@Daniels-MBP.home>
2019-05-25 02:04:28 -0400

    changed app to website

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = (props) => {
             <h2>Full Stack App</h2>
             <h3>Anxiety Manager is a full stack app built on a Ruby On Rails backend with a React frontend,
             designed to help users toward introspecting about their day to day anxieties through data visualization.</h3>
-            <a className="small-link" target="_blank" href="http://anxietymanager.surge.sh">Checkout the app</a>
+            <a className="small-link" target="_blank" href="http://anxietymanager.surge.sh">Website</a>
             <br/>
             <a className="small-link" target="_blank" href="https://github.com/dgillen27/anxiety_manager">Github</a>
             <br/>
@@ -37,7 +37,7 @@ const Projects = (props) => {
             </a>
             <h2>Front End App</h2>
             <h3>For this project, I had the pleasure of working with some very talented to UX designers to build a redesign of an already existing website.</h3>
-            <a className="small-link" target="_blank" href="http://sushizo-redesign.surge.sh">Checkout the app</a>
+            <a className="small-link" target="_blank" href="http://sushizo-redesign.surge.sh">Website</a>
             <br/>
             <a className="small-link" target="_blank" href="https://github.com/dgillen27/sushi_zo_redesign_ux">Github</a>
             <br/>
@@ -59,7 +59,7 @@ const Projects = (props) => {
             <h2>Full Stack App</h2>
             <h3>Beat HostR is built on an Express backend and a React front end, it is a platform for uploading your music and sharing it with others.
             Upload your music here and search through the music of other musicians on the site.</h3>
-            <a className="small-link" target="_blank" href="https://foregoing-celery.surge.sh/">Checkout the app</a>
+            <a className="small-link" target="_blank" href="https://foregoing-celery.surge.sh/">Website</a>
             <br/>
             <a className="small-link" target="_blank" href="https://github.com/BrunoCappelleti/beat-hostR">Github</a>
             <br/>
@@ -82,7 +82,7 @@ const Projects = (props) => {
             <h2>Front End App</h2>
             <h3>Brewery Finder is a React app that consumes the OpenBreweryDB API, it's designed for those who love beer!
             Search for breweries near you and find your next spot to grab some beer!</h3>
-            <a className="small-link" target="_blank" href="http://breweryfinder.surge.sh/">Checkout the app</a>
+            <a className="small-link" target="_blank" href="http://breweryfinder.surge.sh/">Website</a>
             <br/>
             <a className="small-link" target="_blank" href="https://github.com/dgillen27/Brewery-Finder-App">Github</a>
             <br/>
@@ -104,7 +104,7 @@ const Projects = (props) => {
             </a>
             <h2>Front End App</h2>
             <h3>Matchy shapes is an interactive javascript point and click game. Try to change all of the shapes to the same type within the time limit!</h3>
-            <a className="small-link" target="_blank" href="http://matchyshapes.surge.sh/">Checkout the app</a>
+            <a className="small-link" target="_blank" href="http://matchyshapes.surge.sh/">Website</a>
             <br/>
             <a className="small-link" target="_blank" href="https://github.com/dgillen27/Project1_WDI">Github</a>
             <br/>
