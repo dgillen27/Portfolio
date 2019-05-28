90d3d929935a436c7ad891259722a2bb3f8db7ce
Daniel Gillen <danielgillen@Daniels-MBP.home>
2019-05-28 10:47:46 -0400

    updated resume link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = (props) => {
           and people drives me to create the best possible experience
           for users and colleagues alike.</p>
           <div className="about-icon-container">
-            <a target="_blank" href="https://drive.google.com/file/d/1l6nI9ruD8orVsiv7xpmfOsk2_O8AgCoy/view?usp=sharing"><img className="about-icon" src="https://s3.amazonaws.com/image-urls/images/resume-icon-16.jpg" alt="" /></a>
+            <a target="_blank" href="https://drive.google.com/file/d/1pfd2euOcSUqrx49ZjljKSiquVrDSvLf_/view?usp=sharing"><img className="about-icon" src="https://s3.amazonaws.com/image-urls/images/resume-icon-16.jpg" alt="" /></a>
             <a target="_blank" href="https://www.linkedin.com/in/danielgillen/"><img className="about-icon" src="https://s3.amazonaws.com/image-urls/images/square-linkedin-512.png" alt="" /></a>
             <a target="_blank" href="https://github.com/dgillen27"><img className="about-icon" src="https://s3.amazonaws.com/image-urls/images/githubicon.png" alt="" /></a>
           </div>
