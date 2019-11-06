58d14f242485022bbcf395a4aa4be362c0ae38cf
Daniel Gillen <danielbgillen@gmail.com>
2019-11-06 12:55:40 -0500

    removed unnecessary components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component, useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import Contact from "./components/Contact";
